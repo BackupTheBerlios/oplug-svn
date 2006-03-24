ae234fb7bfc22635c2134a6b3835eaa8d7a14b73
matador <matador@78ee023b-370f-0410-835e-d02c856139e3>
2006-03-24 09:41:55 +0000

    Dodalem plik w ktorym powinna zawierac sie lista kanalow do przegladniecia.. (channels.rss)
    W klasie RSS male zmiany:
    Juz konkretna obsluga jednego z najpopularniejszych blogow, czyli 
    Bloggera i jak na razie tylko bloggera..
    
    metoda tworzaca plik wynikowy wciaz nie istnieje...
    Tymczasowo posty wypisywane sa w przegladarce..

--- rss_class.php
+++ rss_class.php
@@ -2,37 +2,59 @@
 // Klasa RSS umozliwia odczytywanie kanalow RSS i przetwarzanie interesujacych nas informacji na HTML.
 // Na razie nie jest to duzo, ale od czegos trzeba zaczac...
 
+//Metody publiczne:
+//	RSS(); - konstruktor;
+//	RSS::print_post($post_id); - wyswietlenio okreslonego posta
+
 class RSS {
-	var $debug = true;
+	var $debug = true; // flaga zarezerwowana
 
 	var $url_base = "http://linux.opole.pl/";
-	var $example_rss_chan = "http://www.linux.pl/rss.php";
+	var $filename = "planeta.php"; // Plik do wygenerowania
+	var $filename_short = "planetka.php"; // plik ze skrotami..
+	var $chanfile = "channels.rss"; // plik z kanalami w postaci URL
+	var $channels = array(); // tablica kanalow do przegladniecia
 	var $version = "1.0";
 
-	var $RSSparser;
+	var $RSSparser; // Maszynka do wyciagania informacji z RSS :]
 
 	var $rssVersion = 2.0;
 	var $rssInfo = array(); // Info na tema feeda
 	var $rssData = array(); // A tutaj wlasnie beda wszystkie informacje wyciagniete z RSS'a
 
-	var $file="data.xml";
-
 	var $curID = -1;
-	var $inChannel = false;
-	var $inItem = false;
-	var $isTitle = false;
-	var $isLink = false;
-	var $isDesc = false;
+	var $inEntry = false;
+	var $isDate = false;
+	var $isEntryTitle = false;
+	var $isDiv = false;
+	var $isID = false;
 
+	
 
 	function RSS() {
 		print "<br/>RSS feed reader v" . $this->version . " by OpLUG<br/>";
-		$this->parse($this->example_rss_chan);
+		print "Now Blogger support only<br/>";
 
-		//przyladowy post
-		print "<br/>".$this->rssInfo["title"]."<br/>";
-		print "<br/><b>".$this->rssData[0]["title"]."</b><br/><br/>";
-		print $this->rssData[0]["description"]."<br/>";
+		$this->get_channels($this->chanfile);
+
+		$this->parse($this->channels[0]);
+		$this->print_to_file($filename);
+
+	}
+
+	function get_channels($chanfile) {
+		
+		$this->channels = file($chanfile);
+		if(!($this->channels[0])) die("Brak kanalow.. Sprawdź plik ".$chanfile."<br/>");
+	return false;
+	}
+
+	function print_to_file($filename) {
+//		if (!($fp = fopen($filename, "a"))) {
+//  		 die("Nie moge otworzyc pliku ".$filename."<br/>");
+//		}
+		print $this->rssData[1]['title'];
+		
 	}
 
 	function parse($channel) {
@@ -40,12 +62,12 @@ class RSS {
 		xml_set_element_handler($this->RSSparser,array(&$this,"startElement"),array(&$this,"endElement"));
 		xml_set_character_data_handler($this->RSSparser, array(&$this, "XMLcharacterData"));
 		if (!($fp = fopen($channel, "r"))) {
-  		 die("could not open XML input");
+  		 die("Nie moge otworzyc kanalu ".$channel.". Zly adres???");
 		}
 
 		while ($data = fread($fp, 4096)) {
    		if (!xml_parse($this->RSSparser, $data, feof($fp))) {
-       	die(sprintf("XML error: %s at line %d",
+       	die(sprintf("Blad XML: %s at line %d",
                    xml_error_string(xml_get_error_code($this->RSSparser)),
                    xml_get_current_line_number($this->RSSparser)));
    		}
@@ -53,58 +75,34 @@ class RSS {
 		xml_parser_free($this->RSSparser);
 	}
 
-	function startElement($parser, $name, $attrs)
-	{
-		if( $name == "rss" || $name == "RSS" ) $this->isRSS = true;
- 
-		if(! $this->isRSS ) $this->errError(16);
- 
-		switch( $name ){
-			case "rss" : $this->rssVersion = $attrs["version"]; break;
-			case "RSS" : $this->rssVersion = $attrs["VERSION"]; break;
-			case "channel" :
-			case "CHANNEL" : $this->inChannel = true; break;
-			case "item" :
-			case "ITEM" : $this->rssData[] = array(); $this->curID++; $this->inItem = true; break;
-			case "title" :
-			case "TITLE" : $this->isTitle = true; break;
-			case "link" :
-			case "LINK" : $this->isLink = true; break;
-			case "description" :
-			case "DESCRIPTION" : $this->isDesc = true; break;
+	function startElement($parser, $name, $attrs) {
+
+		switch($name) {
+			case "GENERATOR" : if($attrs["URL"]!="http://www.blogger.com/") die("To nie jest blogger.. Koncze..<br/>");
+			case "ENTRY" : $this->inEntry = true; break;
+			case "TITLE" : $this->isEntryTitle = true; break;
+			case "DIV" : $this->isDiv = true; break;
+			case "CREATED" : $this->isDate = true; break;
 		}
+ 
+
 	}
 
-	function endElement($parser, $name)
-	{
-		switch( $name ){
-			case "channel" :
-			case "CHANNEL" : $this->inChannel = false; break;
-			case "item" :
-			case "ITEM" : $this->inItem = false; break;
-			case "title" :
-			case "TITLE" : $this->isTitle = false; break;
-			case "link" :
+	function endElement($parser, $name) {
+		switch($name) {
+			case "ENTRY" : $this->inEntry = false; break;
+			case "TITLE" : $this->isEntryTitle = false; break;
+			case "DIV" : $this->isDiv = false; break;
+			case "CREATED" : $this->isDate = false; break;
 			case "LINK" : $this->isLink = false; break;
-			case "description" :
-			case "DESCRIPTION" : $this->isDesc = false; break;
 		}
 	}
 
-function XMLcharacterData($parser, $data){
-	if( $this->inChannel )
-		if(! $this->inItem ){
-			if( $this->isTitle ) $this->rssInfo["title"] = $data;
-			if( $this->isLink ) $this->rssInfo["link"] = $data;
-		}else
-			if( $this->inItem ){
-				$curID = $this->curID;
-			if( $this->isTitle )
-				$this->rssData[$curID]["title"] = $data;
-			if( $this->isLink )
-				$this->rssData[$curID]["link"] = $data;
-			if( $this->isDesc )
-				$this->rssData[$curID]["description"] .= $data;
+	function XMLcharacterData($parser, $data){
+		if($this->inEntry) {
+			if($this->isDate) print "<br/>".$data."<br/>";
+			if($this->isEntryTitle) print "<b>".$data."</b><br/><br/>";
+			if($this->isDiv) print $data."<br/><br/>";
 		}
 	}
 
